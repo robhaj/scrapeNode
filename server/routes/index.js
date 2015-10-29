dbd042142a9c3f9652ee97e839f424e532488a4d
Robert Hajek <hajek.rob@gmail.com>
2015-10-29 16:39:36 -0600

    finished exercised, currently nested, needs callbacks or promises

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var request = require("request");
-var cheerio = require("cheerio");
 var util = require('../scraper.js');
-var js = "javascript";
 
 router.get('/', function(req, res, next) {
   res.send('hello');
--- server/scraper.js
+++ server/scraper.js
@@ -1,12 +1,16 @@
+//dependencies
 var request = require("request");
 var cheerio = require("cheerio");
 
 function callbackHell() {
+
+  //cache urls to scrape
   var mdnUrl = 'https://developer.mozilla.org/en-US/docs/Web/JavaScript';
   var pyUrl = 'https://www.python.org/';
   var hNewsUrl = 'https://news.ycombinator.com';
   var redditUrl = 'https://www.reddit.com/r/Web_Development/';
 
+  //assign booleans before checking string match
   var hackNewsJS = false;
   var redditJS = false;
 
@@ -30,9 +34,7 @@ function callbackHell() {
           redditJS = true;
           return redditJS;
         }
-      }
-
-      //if the word javascript is found in hnews or reddit then scrape mdn
+      } //if the word javascript is found in hnews or reddit then scrape mdn
       if (redditJS === true || hackNewsJS === true) {
 
         //mdn scrape
@@ -42,11 +44,8 @@ function callbackHell() {
             var mdnFun = $('#Join_the_JavaScript_community').text();
             console.log(mdnFun);
           }
-        });
-
-        //otherwise scrape python.org
+        }); //otherwise scrape python.org
       } else {
-
         //python.org scrape
         request(pyUrl, function(error, response, html) {
           if (!error && response.statusCode == 200) {
@@ -54,11 +53,11 @@ function callbackHell() {
             var pyFun = $('#success-story-2 > blockquote > a').text();
             console.log(pyFun);
           }
-        });
-      }
-    });
-  });
-}
+        }); //end python.org scrape request
+      } //end else conditional
+    }); //end reddit scrape request
+  }); //end hnews scrape request
+} //end callbackHell function
 
 module.exports = {
   callbackHell:callbackHell
